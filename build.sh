b512312a3bb8d07690a8b1f3eca197e7aa610cb7
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2023-12-01 21:09:20 -0600

    test: update docs workflow

--- build.sh
+++ build.sh
@@ -4,19 +4,26 @@ me="$(basename ${BASH_SOURCE[0]})"
 function help() {
   cat <<- EOH
 
-		$me <docs repo> <core repo>
+		$me <docs dir> <core dir> <current release>
+
+		Example:
+		  ./$me ./docs ./core v0.18.0
 
 	EOH
 }
 
-docs_repo="$1" ; shift 1
-if [ -z "$docs_repo" ] ; then help ; echo "Required: <docs repo>"$'\n' ; exit 1 ; fi
-docs_repo=$( realpath "$docs_repo" )
+docs_dir="$1" ; shift 1
+if [ -z "$docs_dir" ] ; then help ; echo "Required: <docs dir>"$'\n' ; exit 1 ; fi
+docs_dir=$( realpath "$docs_dir" )
 
-core_repo="$1" ; shift 1
-if [ -z "$core_repo" ] ; then help ; echo "Required: <core repo>"$'\n' ; exit 1 ; fi
-core_repo=$( realpath "$core_repo" )
+core_dir="$1" ; shift 1
+if [ -z "$core_dir" ] ; then help ; echo "Required: <core dir>"$'\n' ; exit 1 ; fi
+core_dir=$( realpath "$core_dir" )
+
+curr_rel="$1" ; shift 1
+if [ -z "$curr_rel" ] ; then help ; echo "Required: <current release>"$'\n' ; exit 1 ; fi
 
 
-echo "  Docs repo path: \"$docs_repo\""
-echo "  Core repo path: \"$core_repo\""
+echo "  Docs repo path: \"$docs_dir\""
+echo "  Core repo path: \"$core_dir\""
+echo "  Core release:   \"$curr_rel\""
