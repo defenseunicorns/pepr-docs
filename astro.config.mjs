7be85561e4bf25a9e1aeec9ed92aff5b32369407
Amber <amber.j.fryar@gmail.com>
2025-09-17 09:28:49 -0600

    chore: test other version

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
 				starlightLlmsTxt(),
 				starlightVersions({
 					versions: [
-						{ slug: 'v0.54', label: 'v0.54.0' },
+						{ slug: 'v0.53', label: 'v0.53.1' },
 					],
 					current: { label: 'Latest' },
 				}),
